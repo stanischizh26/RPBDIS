6a56730a502ff993abcb4c4cef13bad8488cc4cb
Stas <stanischizh9391@mail.ru>
2024-10-22 21:13:33 +0300

    Добавьте файлы проекта.

---
+++ Similar_products/Models/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Similar_products.Models;
+
+public partial class ProductType
+{
+    public int ProductTypeId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int ProductId { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+}
